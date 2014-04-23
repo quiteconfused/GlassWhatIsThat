864d8bec370ea3274acb42f3a71711eaa2d2a713
Robert Weikel <quiteconfused@gmail.com>
2014-04-22 23:09:18 -0400

    Added xe16 support

--- src/com/confused/glass/whatisthat/WhatIsThatActivity.java
+++ src/com/confused/glass/whatisthat/WhatIsThatActivity.java
@@ -2,9 +2,7 @@ package com.confused.glass.whatisthat;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 import org.opencv.android.OpenCVLoader;
@@ -14,7 +12,6 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
-import org.opencv.core.Range;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -24,7 +21,7 @@ import org.opencv.imgproc.Imgproc;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.speech.tts.TextToSpeech;
@@ -36,14 +33,14 @@ import com.confused.glass.whatisthat.GlassSnapshotActivity;
 import com.confused.instarename.post.MultipartPost;
 import com.confused.instarename.post.PostParameter;
 import com.google.android.glass.app.Card;
-import com.google.android.glass.timeline.TimelineManager;
+
+
+
 
 /**
  * Main activity.
  */
 public class WhatIsThatActivity extends Activity {
-	
-  private static TimelineManager tlm;
 	
   private static Card _card;
   private static View _cardView;
@@ -59,11 +56,12 @@ public class WhatIsThatActivity extends Activity {
   private boolean picTaken = false; // flag to indicate if we just returned from the picture taking intent
   protected boolean mbActive;
 
+  /*
   public static void updateResultsInUi(Context context, String results, File image){
 	  
 	  Card card = new Card(context);
 	  card.setImageLayout(Card.ImageLayout.FULL);
-	  card.addImage( Uri.fromFile(image));
+	  card.addImage( BitmapFactory.decodeFile(image.getAbsolutePath()));
 
 	  if(results!=null){
 		  card.setFootnote("Image Info: " + results);
@@ -73,8 +71,17 @@ public class WhatIsThatActivity extends Activity {
 		  _speech.speak("No results found." , TextToSpeech.QUEUE_FLUSH, null);
 	  }
 	  
-	  tlm.insert(card);
+	  _cardView = card.getView();
+
+	  
+	  WhatIsThatActivity.this.runOnUiThread(new Runnable() {
+		  	public void run(){
+		  		setContentView(_cardView);
+		  	}
+	  });
+	  
   }
+  */
   
 
   private static String sendPostRequest(File filename) {
@@ -175,7 +182,7 @@ public class WhatIsThatActivity extends Activity {
     // Even though the text-to-speech engine is only used in response to a menu action, we
     // initialize it when the application starts so that we avoid delays that could occur
     // if we waited until it was needed to start it up.
-    tlm = TimelineManager.from(_context);getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     
     
     
@@ -190,7 +197,7 @@ public class WhatIsThatActivity extends Activity {
     _card = new Card(_context);
     _card.setFootnote("Processing Image");
     
-    _cardView = _card.toView();
+    _cardView= _card.getView();
     _cardView.setKeepScreenOn(true);
     setContentView(_cardView);
 
@@ -228,7 +235,7 @@ public class WhatIsThatActivity extends Activity {
     case KeyEvent.KEYCODE_ENTER:
 
       // Change the text of the card when the touchpad is touched
-      _cardView = _card.toView();
+      _cardView = _card.getView();
       setContentView(_cardView);
 
       // Status message below the main text in the alternative UX layout
@@ -274,11 +281,11 @@ public class WhatIsThatActivity extends Activity {
 		    	    
 		    	    performOnBackgroundThreadSingleFile(_context, f);
 		    		_card.setImageLayout(Card.ImageLayout.FULL);
-		    		_card.addImage( Uri.fromFile(f));
+		    		_card.addImage(BitmapFactory.decodeFile(f.getAbsolutePath()));
 		    		_card.setFootnote("processing image");
 		    		_cardView.invalidate();
 		    		
-		    		_cardView = _card.toView();
+		    		_cardView = _card.getView();
 		    		setContentView(_cardView);
 		    		
 		    		Mat m = Highgui.imread(f.getAbsolutePath());
@@ -365,7 +372,7 @@ public class WhatIsThatActivity extends Activity {
 	  }
 	}
 	
-    public static Thread performOnBackgroundThreadSingleFile(final Context context, final File f) {
+    public Thread performOnBackgroundThreadSingleFile(final Context context, final File f) {
         final Thread t = new Thread() {
             @Override
             public void run() {
@@ -376,7 +383,18 @@ public class WhatIsThatActivity extends Activity {
 	        		Log.d("Stuff", "Result is: " + new_result);                
 
                 } finally {
-                	updateResultsInUi(context, new_result, f);
+            	  if(new_result!=null){
+                  	  final Card card = new Card(context);
+                	  card.setImageLayout(Card.ImageLayout.FULL);
+                	  card.addImage( BitmapFactory.decodeFile(f.getAbsolutePath()));
+            		  card.setFootnote("Image Info: " + new_result);
+            		  _speech.speak("You are viewing " + new_result, TextToSpeech.QUEUE_FLUSH, null);
+            		  WhatIsThatActivity.this.runOnUiThread(new Runnable() {
+	              		  	public void run(){
+	              		  		setContentView( card.getView());
+	              		  	}
+	              	  });
+            	  }
                 }
             }
         };
